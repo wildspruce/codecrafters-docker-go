4567b69bb736b7d4fc030268fc26cf488459f973
Elya.Mamedova <elya.mamedova@jetbrains.com>
2024-07-23 19:50:18 +0200

    [3 stage] Implement chroot environment for command execution

--- app/main.go
+++ app/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"os"
 	"os/exec"
+	"path/filepath"
+	"syscall"
 )
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
@@ -12,10 +14,31 @@ func main() {
 
 	cmd := exec.Command(command, args...)
 
+	// to avoid an error `open /dev/null: no such file or directory`
+	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	err := cmd.Run()
+	tmpDir := "/tmp/myfakehost"
+	err := os.MkdirAll(tmpDir, 0744)
+	if err != nil {
+		panic("Failed to create tmp dir: " + err.Error())
+	}
+
+	err = exec.Command("mkdir", "-p", filepath.Join(tmpDir, filepath.Dir(command))).Run()
+	if err != nil {
+		panic("Command mkdir failed: " + err.Error())
+	}
+
+	err = exec.Command("cp", command, filepath.Join(tmpDir, command)).Run()
+	if err != nil {
+		panic("Command copy failed: " + err.Error())
+	}
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Chroot: tmpDir,
+	}
+
+	err = cmd.Run()
 	if err != nil {
 		exitCode := cmd.ProcessState.ExitCode()
 		os.Exit(exitCode)
